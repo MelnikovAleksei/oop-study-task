8f5bd2affa9fb38258cd43f9c0874468016d6ffb
MelnikovAleksei <melnikov.aleksei.alexeyevich@gmail.com>
2020-02-11 20:19:57 +0300

    created const, random(min, max)

---
+++ bouncing-balls/main.js
@@ -0,0 +1,10 @@
+let canvas = document.querySelector('canvas');
+let ctx = canvas.getContext('2d');
+
+let width = canvas.width = window.innerWidth;
+let height = canvas.height = window.innerHeight;
+
+function random(min, max) {
+    const num = Math.floor(Math.random() * (max - min + 1)) + min;
+    return num;
+}
